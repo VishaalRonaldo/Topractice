24a8dd50db8eb626ee79a4d13f3b59b3fd0b685c
vishnudevans <vd361533@gmail.com>
2026-05-07 19:39:34 +0530

    first practice

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("The animal is eating");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("The dog is barking");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+
+        dog.Eat();   // Inherited from Animal
+        dog.Bark();  // Dog's own method
+    }
+}
